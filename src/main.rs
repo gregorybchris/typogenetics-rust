f1c13d2d3d2c9345d23995d73bd2179bfb1dd332
Chris Gregory <gregorybchris@gmail.com>
2024-01-15 19:26:51 -0800

    Basic translation and rewriting in clap CLI

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,72 @@ mod rewriter;
 mod strand;
 mod translator;
 mod turn;
-use crate::{rewriter::Rewriter, strand::Strand, translator::Translator};
+use crate::{enzyme::Enzyme, rewriter::Rewriter, strand::Strand, translator::Translator};
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Translate a strand into a list of enzymes
+    Translate {
+        /// Strand to translate
+        strand_str: String,
+
+        /// Whether to emit debug logs
+        #[arg(long, default_value_t = false)]
+        debug: bool,
+    },
+
+    /// Rewrite a strand using an enzyme
+    Rewrite {
+        /// Enzyme to apply to strand
+        enzyme_str: String,
+
+        /// Strand to rewrite with enzyme
+        strand_str: String,
+
+        /// Whether to emit debug logs
+        #[arg(long, default_value_t = false)]
+        debug: bool,
+    },
+}
 
 fn main() {
-    let strand = Strand::from_string("TACATTATCAGGGGGTATAGGTATACCCGGTGGTAACATATCCT");
-    println!("{}", strand);
-    let enzymes = Translator::translate(&strand);
-    println!("{:?}", enzymes);
-    if let Some(enzyme) = enzymes.get(0) {
-        let new_strands = Rewriter::rewrite(enzyme, &strand);
-        println!("{:?}", new_strands);
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Translate {
+            strand_str,
+            debug: _,
+        }) => {
+            let strand = Strand::from_string(strand_str);
+            let enzymes = Translator::translate(&strand);
+            for enzyme in enzymes {
+                println!("{}", enzyme);
+            }
+        }
+        Some(Commands::Rewrite {
+            enzyme_str,
+            strand_str,
+            debug: _,
+        }) => {
+            let enzyme = Enzyme::from_string(enzyme_str);
+            let strand = Strand::from_string(strand_str);
+            let new_strands = Rewriter::rewrite(&enzyme, &strand);
+            for new_strand in new_strands {
+                println!("{}", new_strand);
+            }
+        }
+        None => {
+            println!("Default subcommand");
+        }
     }
 }
